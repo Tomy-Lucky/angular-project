c3ab8046f6b51cef7301fbe1bfb916812b85b43a
tomy_lucky <ase3577@gmail.com>
2021-03-08 20:46:30 +0600

    Staring project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DiaryComponent } from './components/diary/diary.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DiaryComponent
   ],
   imports: [
     BrowserModule,
