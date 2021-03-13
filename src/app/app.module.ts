b88076c5f22b2f7a8cf8b8ead640d915b01ad2f8
tomy_lucky <ase3577@gmail.com>
2021-03-13 14:33:52 +0600

    Prepared main page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,33 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
+import {RouterModule, Routes} from '@angular/router';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { DiaryComponent } from './components/diary/diary.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {DiaryComponent} from './components/diary/diary.component';
+import {ContactsComponent} from './components/contacts/contacts.component';
+
+const appRoutes: Routes = [
+  {path: '', component: DiaryComponent},
+  {path: 'contacts', component: ContactsComponent}
+];
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    DiaryComponent
+    DiaryComponent,
+    ContactsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/components/diary/diary.component.ts
+++ src/app/components/diary/diary.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-diary',
@@ -7,9 +7,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DiaryComponent implements OnInit {
 
-  constructor() { }
+  studentName: string;
+  internship: Internship;
+  options: string[];
+  isEditDescription: boolean;
 
-  ngOnInit(): void {
+  constructor() {
+    this.studentName = 'Student';
+    this.internship = {
+      companyName: 'Kolesa',
+      shortInfo: 'learn to develop backend application',
+      typeOfWork: 'Project',
+      periodInDays: 30
+    };
+    this.options = ['skills improvement', 'learn new backend topics', 'maintain real world app'];
+    this.isEditDescription = false;
   }
 
+  ngOnInit(): void {
+
+  }
+
+  selectTypeOfWork(typeOfWork: string): void {
+    this.internship.typeOfWork = typeOfWork;
+  }
+
+  addOption(option: string): boolean {
+    this.options.unshift(option);
+    return false;
+  }
+
+  deleteOption(option: string): void {
+    for (let i = 0; i < this.options.length; i++) {
+      if (this.options[i] === option) {
+        this.options.splice(i, 1);
+        break;
+      }
+    }
+  }
+
+  showEdit(): void {
+    this.isEditDescription = !this.isEditDescription;
+  }
+}
+
+interface Internship {
+  companyName: string;
+  shortInfo: string;
+  typeOfWork: string;
+  periodInDays: number;
 }
